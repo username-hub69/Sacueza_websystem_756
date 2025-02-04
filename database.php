f46932703df29f10ff908b1bd58bf380ec5c3cc4
Amadheus <jbsacueza000@yahoo.com>
2025-02-04 11:49:34 +0800

    .
    Commit

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "sacueza_756";
+
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if($conn->connect_error){
+    die("Connection Failed". $conn->connect_error);
+}
+
+?>
